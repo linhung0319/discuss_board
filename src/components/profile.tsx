9b254fb471643126f3194cad165fa526f17676e5
linhung0319 <linhung0319@gmail.com>
2024-07-30 17:00:10 +0800

    Create discuss board project

---
+++ src/components/profile.tsx
@@ -0,0 +1,14 @@
+// src/components/profile.tsx
+"use client";
+
+import { useSession } from "next-auth/react";
+
+export default function Profile() {
+  const session = useSession();
+
+  if (session.data?.user) {
+    return <div>From client: {JSON.stringify(session.data.user)}</div>;
+  }
+
+  return <div>From client: user is signed out</div>;
+}
